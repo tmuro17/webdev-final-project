808aa702cf23e4ea2340e675e6fd36d8e7b9f2b3
Tanner Muro <tmuro17@gmail.com>
2021-04-08 20:12:46 -0400

    PRODUCTIZE...

--- backend/config/prod.secret.exs
+++ backend/config/prod.secret.exs
@@ -35,7 +35,7 @@ config :backend_final, BackendWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-config :backend, BackendWeb.Endpoint, server: true
+config :backend_final, BackendWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
